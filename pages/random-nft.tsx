41a1009f3f543685e617deec741c293a42b746f5
Shain Dholakiya <shainshain99@gmail.com>
2022-04-11 07:20:05 -0700

    code clean up

--- pages/random-nft.tsx
+++ pages/random-nft.tsx
@@ -98,11 +98,6 @@ const RandomNFT = () => {
               <div className="font-bold text-xl mb-2">{winner.name}</div>
               <p className="text-gray-700 text-base">{winner.description}</p>
             </div>
-            {/* <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-          </div> */}
           </div>
         ) : null}
 
